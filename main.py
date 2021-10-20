d08bdd621b17cf40ba85fbaefb74dbb9fea557f8
Maxim <gaiiiuiiialikeaboos@gmail.com>
2021-10-20 23:03:09 +0300

    added some structure

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import pygame as pg
+import os
+import sys
+
+
+class SpaceWars:
+    def __init__(self):
+        print('path is ', os.path.dirname(__file__))
+
+    def run(self):
+        pass
 
 
 def main():
-    pass
+    sw = SpaceWars()
+    sw.run()
 
 
 if __name__ == '__main__':
